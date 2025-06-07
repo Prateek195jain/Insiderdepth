83c3be1d708a9b70eca9a8e4d615319340b86a89
Umar Khan <khanumar0108@gmail.com>
2025-06-07 23:58:20 +0530

    fix: Update params type to promise for SingleJournalPage component

--- app/journal/[journal-name]/page.tsx
+++ app/journal/[journal-name]/page.tsx
@@ -3,7 +3,7 @@ import JournalPageComponent from "@/features/journal/page/single-journal-page";
 const SingleJournalPage = async ({
   params,
 }: {
-  params: { "journal-name": string };
+  params: Promise<{ "journal-name": string }>;
 }) => {
   const resolvedParams = await params;
   return <JournalPageComponent journalName={resolvedParams["journal-name"]} />;
